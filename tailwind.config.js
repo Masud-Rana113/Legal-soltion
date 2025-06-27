a1f53852f8bf2e44eb7415571842e0cc161ce60a
Masud-Rana113 <107640718+Masud-Rana113@users.noreply.github.com>
2025-06-27 09:02:29 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./index.html"], // or other paths where your HTML lives
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['"Poppins"', "sans-serif"],
+        ebgaramond: ['"EB Garamond"', "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
